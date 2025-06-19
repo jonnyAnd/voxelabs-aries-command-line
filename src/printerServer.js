ff6ae39635a0d751feb9135fe7ded07f8a0a5055
jonnyAnd <jonny@eino.co.uk>
2025-06-19 02:51:45 +0100

    web interface

--- src/printerServer.js
+++ src/printerServer.js
@@ -1,5 +1,6 @@
 // printerServer.js
 const http = require('http');
+const WebInterface = require('./webInterface');
 
 class PrinterServer {
     constructor(printer, port = 1337) {
@@ -9,17 +10,25 @@ class PrinterServer {
 
     start() {
         const server = http.createServer((req, res) => {
-            if (req.method === 'GET' && req.url === '/status') {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(this.printer));
+            if (req.method === 'GET') {
+                if (req.url === '/status') {
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(this.printer));
+                } else if (req.url === '/') {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(WebInterface.getHomePageHTML());
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('404 Not Found');
+                }
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Not Found');
+                res.writeHead(405, { 'Content-Type': 'text/plain' });
+                res.end('405 Method Not Allowed');
             }
         });
 
         server.listen(this.port, () => {
-            console.log(`🚀 PrinterServer running at http://localhost:${this.port}/status`);
+            console.log(`🌐 PrinterServer listening on http://localhost:${this.port}`);
         });
     }
 }
